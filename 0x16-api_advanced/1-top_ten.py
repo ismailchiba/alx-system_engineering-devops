3996915bafd7865168ffcc997ccc669ce54655ae
root <root@DESKTOP-A0SGFGQ>
2024-08-14 18:07:25 +0100

    api advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API
+and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'user_agent': 'Mozilla/5.0'}
+    params = {'limit': 10}
+    response = get(url, headers=headers, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
