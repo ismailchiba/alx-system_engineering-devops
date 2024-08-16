0fe928cbefd2d5d814fcc235121c834ed56e8a43
= <=>
2024-08-16 19:31:55 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """ function that queries the Reddit API
-and returns the number of subscribers
-"""
+and returns the number of subscriber """
+
 from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """function"""
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {
-        'User-agent': 'Google Chrome Version 81.0.4044.129'
-    }
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
